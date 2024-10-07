1baf95dcb8555a2cea2e869536352d64b8c2e35d
janghs <gorae930301@gmail.com>
2024-10-07 14:21:37 +0900

    Basic

---
+++ src/main/java/org/barcord/barcode_service/BarcodeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.barcord.barcode_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcodeServiceApplication.class, args);
+	}
+
+}
